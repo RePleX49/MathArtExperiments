8d8ecbbf6d1cab16009fdcc401ab8c89d5d69dae
RePleX49 <lukelin2009@hotmail.com>
2020-02-13 22:13:02 -0500

    Added Moving Weave

--- Assets/Scripts/DrawPixels.cs
+++ Assets/Scripts/DrawPixels.cs
@@ -8,18 +8,39 @@ public class DrawPixels : MonoBehaviour
     RawImage pixelImage;
     Texture2D drawing;
 
+    /* Nice Patterns
+     * "a", "a", "a", "b", "a", "b", "b", "b", "a", "c", "a", "c", "c", "c", "b", "c", "b", "c"
+     * "a", "a", "a", "b", "a", "b", "a", "c", "a", "c", "b", "c", "b", "c", "b", "b", "c", "c"  
+     * aabbccaaabbbccc
+     * accbbbcca (SwapIndex = 3)
+     * aabbccddeeffaabbccddeeff (SwapIndex = 8)
+     * aabbccddeeffaabbccddeeff (SwapIndex = 10) (Speed = 0.05)
+     */
+
+    public string weavePattern = "aabbccaaabbbccc";
+
+    public float ShiftSpeed = 0.02f;
+
+    // index that ShiftString function will use to swap the final character
+    [Range(0, 14)]
+    public int SwapIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupTexture();
 
         AdaWeave();
+
+        InvokeRepeating("ShiftString", 0.0f, ShiftSpeed);
+        InvokeRepeating("AdaWeave", 0.0f, ShiftSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
         // RandColor();
+        
     }
 
     void RandColor()
@@ -62,13 +83,7 @@ public class DrawPixels : MonoBehaviour
     }
 
     void AdaWeave()
-    {
-        //"a", "a", "a", "b", "a", "b", "b", "b", "a", "c", "a", "c", "c", "c", "b", "c", "b", "c"
-
-        //"a", "a", "a", "b", "a", "b", "a", "c", "a", "c", "b", "c", "b", "c", "b", "b", "c", "c" 
-
-        string weavePattern = "aaababacacbcbcbbcc";
-
+    {    
         Color[] colorPattern = new Color[drawing.height * drawing.width];
 
         for (int x = 0; x < drawing.width; x++)
@@ -83,12 +98,34 @@ public class DrawPixels : MonoBehaviour
                 else
                 {
                     // no match set cell white
-                    colorPattern[(x*drawing.height) + y] = Color.green;
+                    colorPattern[(x * drawing.height) + y] = Color.yellow;
                 }
             }
         }
 
         drawing.SetPixels(colorPattern);
         drawing.Apply();
+    }
+
+    void ShiftString()
+    {
+        // NewString array will have the shifted pattern chars added into it
+        char[] NewString = new char[weavePattern.Length];
+
+        for (int i = 0; i < weavePattern.Length; i++)
+        {
+            if(i == weavePattern.Length - 1)
+            {
+                // make the last element of NewString the SwapIndex from the weave pattern
+                NewString[i] = weavePattern[SwapIndex];
+            }
+            else
+            {
+                // Shift letters down by one space
+                NewString[i] = weavePattern[i + 1];
+            }
+        }
+
+        weavePattern = new string(NewString);
     }
 }
